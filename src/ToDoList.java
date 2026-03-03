0aaa2f0752e5f4bba4fa09941a41a192369625a6
bivol <vbivol394@gmail.com>
2026-03-03 13:13:05 +0200

    Importat librarie gson salvare in json file

--- src/ToDoList.java
+++ src/ToDoList.java
@@ -1,9 +1,21 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
+import com.google.gson.Gson;
 
 public class ToDoList {
     private ArrayList<String> tasks = new ArrayList<>();
+    private static final String FILE = "tasks.json";
+    private Gson gson = new Gson();
+    public ToDoList(){
+        load();
+    }
+
     public void add(String task){
         tasks.add(task);
+        save();
     }
     public void delete(int index){
         if(index >= 0 && index < tasks.size()){
@@ -13,9 +25,27 @@ public class ToDoList {
     public void list(){
         for (int i = 0;i< tasks.size();i++){
             System.out.println(i + ": "+ tasks.get(i));
+            save();
         }
     }
     public void clear(){
         tasks.clear();
+    }
+    private void save(){
+        try(FileWriter writer = new FileWriter(FILE)){
+            gson.toJson(tasks,writer);
+        }catch (IOException e){
+            System.out.println("Erroare la salvare: " + e.getMessage());
+        }
+    }
+    private void load(){
+        try (FileReader reader = new FileReader(FILE)){
+            tasks = gson.fromJson(reader,ArrayList.class);
+            if (tasks == null ) tasks = new ArrayList<>();
+        }catch (FileNotFoundException e){
+            tasks = new ArrayList<>();
+        } catch (IOException e){
+            System.out.println("Erroare la salvare: " + e.getMessage());
+        }
     }
 }
