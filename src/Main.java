5a1e1ecb300a83ce5a9842134671c7f6dd8131ad
bivol <vbivol394@gmail.com>
2026-03-03 12:37:30 +0200

    Main java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,21 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        int a = 3;
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i+a);
+        ToDoList list  = new ToDoList();
+        Map<String,Command> commands = new HashMap<>();
+        commands.put("add",new AddCommand());
+        commands.put("delete",new DeleteCommand());
+        if (args.length > 0){
+            Command cmd = commands.get(args[0]);
+            if (cmd != null) {
+                cmd.execute(list, args);
+            }else{
+                System.out.println("Commanda necunoscuta: "+ args[0]);
+            }
+        }else{
+            System.out.println("Nici o commanda  introdusa!");
         }
     }
 }
